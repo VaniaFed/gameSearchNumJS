df84eb6228102dc6f3850f496f133fcc45094586
VaniaFed <ztankist@gmail.com>
2018-06-03 19:48:54 +0300

    webpack 4, animation of rotate element

---
+++ src/js/index.js
@@ -0,0 +1,261 @@
+'use strict';
+
+window.onload = function () {
+    const randNum = function (min, max) {
+        let randNum = min - 0.5 + Math.random() * (max - min + 1);
+        randNum = Math.round(randNum);
+        return randNum;
+    };
+
+    const fillArray = function (arr, sizeTable) {
+        for (let i = sizeTable.countLines * sizeTable.countColumns - 1; i >= 0; i--) {
+            if (currentLevel <= 2) {
+                arr[i] = randNum(10, 99);
+            }
+            else if (currentLevel <= 4) {
+                arr[i] = randNum(100, 999);
+            } else {
+                arr[i] = randNum(1000, 9999);
+            }
+        }
+    };
+
+    const changeStyles = function (el) {
+        el.forEach(function (item, i, arr) {
+            if (currentLevel <= 3) {
+                item.style.fontSize = '40px';
+                item.style.padding = '10px 0';
+            }
+            else if (currentLevel === 4) {
+                item.style.fontSize = '30px';
+                item.style.padding = '12px 0';
+            }
+            else if (currentLevel === 5) {
+                item.style.fontSize = '30px';
+                item.style.padding = '15px 0';
+
+            } else {
+                item.style.fontSize = '26px';
+                item.style.padding = '12px 0';
+            }
+        });
+    };
+
+    const selectAction = function () {
+        const animationContent = [
+            'scale_element .5s infinite alternate ease-in-out',
+            'color_element_blue .5s infinite alternate ease-in-out',
+            'color_element_purple .5s infinite alternate ease-in-out',
+            'color_element_green .5s infinite alternate ease-in-out',
+            'color_element_black .5s infinite alternate ease-in-out',
+            'scale_element .5s infinite alternate ease-in-out',
+            'scale_element .5s infinite alternate ease-in-out',
+            'scale_element .5s infinite alternate ease-in-out',
+            'scale_element .5s infinite alternate ease-in-out',
+            'rotate_element .25s infinite alternate ease-in-out'
+        ],
+        colors = [
+            '#2196F3',
+            '#009688',
+            '#673AB7',
+            '#333'
+        ];
+        let el = document.querySelectorAll('.item__num');
+        el.forEach(function(item, i, el) {
+            let currentAction = randNum(1, 7);
+            switch (currentAction) {
+                //only scale
+                case 1: {
+                    item.style.animation = animationContent[0];
+                    break;
+                }
+                case 2: {
+                    let numAnimation = randNum(1, 4);
+                    item.style.animation = animationContent[numAnimation];
+                    break;
+                }
+                case 3: {
+                    let numAnimation = randNum(5, 8);
+                    item.style.animation = animationContent[numAnimation];
+
+                    let numColor = randNum(0, 3);
+                    item.style.background = colors[numColor];
+                    item.style.color = '#fff';
+                    break;
+                }
+                case 4: {
+                    item.style.animation = animationContent[9];
+
+                    let numColor = randNum(0, 3);
+                    item.style.background = colors[numColor];
+                    item.style.color = '#fff';
+                    break;
+                }
+                default: break;
+            }
+        });
+    };
+
+    const resizeTable = function (sizeTable, level) {
+        if (level < 4) {
+            sizeTable.countColumns = 3;
+            sizeTable.countLines = 2;
+        } else if (level < 6) {
+            sizeTable.countColumns = 4;
+            sizeTable.countLines = 3;
+        } else {
+            sizeTable.countColumns = 4;
+            sizeTable.countLines = 4;
+        }
+    };
+
+    const createItem = function (inner, className, parent) {
+        let el = document.createElement('div');
+        el.classList.add(className);
+        el.innerHTML = inner;
+        parent.appendChild(el);
+    };
+
+    const draw = function (sizeTable, arrayNums, mainNumber) {
+        const drowMainNum = function (mainNumber) {
+            let mainNumberEl = document.querySelector('.current_num');
+
+            mainNumberEl.innerHTML = mainNumber;
+        };
+
+        const clearTable = function () {
+            let container = document.querySelector('.container__work__inner');
+            container.innerHTML = '';
+        };
+
+        clearTable();
+
+        drowMainNum(mainNumber);
+
+        let currentPositionArray = 0;
+        for (let i = 0; i < sizeTable.countLines; i++) {
+            let container = document.getElementsByClassName('container__work__inner'),
+                elementRow = document.createElement('div');
+
+            elementRow.classList.add('item__row');
+            container[0].appendChild(elementRow);
+
+            for (let j = 0; j < sizeTable.countColumns; j++) {
+                createItem(arrayNums[currentPositionArray++], 'item__num', elementRow);
+            }
+        }
+    };
+
+    const checkCorrectly = function () {
+        let scoreEl = document.querySelector('.score');
+        if (+this.textContent === mainNumber) {
+            successfullyPressed(scoreEl);
+        } else {
+            failedPressed(scoreEl);
+        }
+        this.removeEventListener('click', checkCorrectly);
+        followTheTable();
+    };
+
+    const increaseScore = function (scoreEl) {
+        scoreVal += 100;
+        scoreEl.innerHTML = 'Score: ' + scoreVal;
+    };
+
+    const reduceScore = function (scoreEl) {
+        if (scoreVal >= 100) {
+            scoreVal -= 100;
+        } else {
+            scoreVal = 0;
+        }
+
+        scoreEl.innerHTML = 'Score: ' + scoreVal;
+    };
+
+    const successfullyPressed = function (scoreEl) {
+        currentLevel++;
+        increaseScore(scoreEl);
+        nextLevel();
+    };
+
+    const failedPressed = function (scoreEl) {
+        currentLevel > 1 ? currentLevel-- : currentLevel;
+        reduceScore(scoreEl);
+        nextLevel();
+    };
+
+    const nextLevel = function () {
+        resizeTable(sizeTable, currentLevel);
+        fillArray(randomNumbers, sizeTable);
+
+
+        mainNumber = randomNumbers[Math.round(Math.random() * (sizeTable.countColumns * sizeTable.countLines - 1))];
+
+        draw(sizeTable, randomNumbers, mainNumber);
+
+        let items = document.querySelectorAll('.item__num');
+        changeStyles(items);
+        selectAction();
+    };
+
+    // функция-костыль
+    const followTheTable = function () {
+        let items = document.getElementsByClassName('item__num');
+
+        for (let i = 0; i < items.length; i++) {
+            items[i].addEventListener('click', checkCorrectly);
+        }
+    };
+
+    const timer = function (start) {
+        let currentIteration = typeof(start) === undefined ? 0 : start;
+
+        return function (f) {
+            let callback = f || function () {
+            };
+
+            if (currentIteration === 0) {
+                callback();
+                currentIteration = start;
+            }
+            return --currentIteration;
+        };
+    };
+
+    const gameOver = function () {
+        let resetNumbers = function () {
+            scoreVal = 0;
+            document.querySelector('.score').innerHTML = 'Score: ' + scoreVal;
+            currentLevel = 1;
+            sizeTable.countColumns = 0;
+            sizeTable.countLines = 0;
+        };
+
+        //modalFinishShow ();
+        resetNumbers();
+    };
+
+    const followTheTimer = function () {
+        let counter = timer(45),
+            timerEl = document.querySelector('.timer');
+
+        setInterval(function () {
+            timerEl.innerHTML = 'Timer: ' + counter(gameOver);
+        }, 1000);
+    };
+
+    let randomNumbers = [],
+        mainNumber,
+        currentLevel = 1,
+        scoreVal = 0,
+        sizeTable = {
+            countColumns: 0,
+            countLines: 0
+        };
+
+    nextLevel();
+
+    followTheTable();
+
+    followTheTimer();
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,139 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const merge = require('webpack-merge');
-const pug = require('./webpack/pug');
-const devserver = require('./webpack/devserver');
-const sass = require('./webpack/sass');
-const css = require('./webpack/css');
-const extractCSS = require('./webpack/css.extract');
-const uglifyJS = require('./webpack/js.uglify');
-const images = require('./webpack/images');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const fs = require('fs')
 
-const PATHS = {
-    source: path.join(__dirname, 'source'),
-    build: path.join(__dirname, 'build')
-};
+function generateHtmlPlugins(templateDir) {
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+    return templateFiles.map(item => {
+        const parts = item.split('.');
+        const name = parts[0];
+        const extension = parts[1];
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+            inject: false,
+        })
+    })
+}
 
-const common = merge([
-    {
-        entry: {
-            'index': PATHS.source + '/pages/index/index.js'
-        },
-        output: {
-            path: PATHS.build,
-            filename: 'js/[name].js'
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                filename: 'index.html',
-                chunks: ['index', 'common'],
-                template: PATHS.source + '/pages/index/index.pug'
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'common'
-            }),
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery'
-            })
+const htmlPlugins = generateHtmlPlugins('./src/html/views');
+
+const conf = {
+    entry: [
+        './src/js/index.js',
+        './src/sass/index.sass',
+    ],
+    output: {
+        filename: 'index.js',
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    },
+                ],
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {name: 'img/[name].[ext]'}
+                    }
+                ]
+            },
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, 'src/sass'),
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            minimize: true,
+                            url: false
+                        }
+                    },
+                    {
+                        loader: "resolve-url-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }]
+                })
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+            },
+            // {
+            //     test: /\.html$/,
+            //     include: path.resolve(__dirname, 'src/html/includes'),
+            //     use: ['raw-loader']
+            // },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
         ]
     },
-    pug(),
-    images()
-]);
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            //template: './src/index.html',
+            template: path.join(__dirname,'src') + '/index.pug',
+        }),
+        new ExtractTextPlugin({
+            filename: './css/index.css',
+            allChunks: true,
+        }),
+        new CopyWebpackPlugin(
+        [
+            {
+                from: './src/fonts',
+                to: './fonts'
+            },
+            {
+                from: './src/favicon',
+                to: './favicon'
+            },
+            {
+                from: './src/img',
+                to: './img'
+            },
+            {
+                from: './src/json',
+                to: './json'
+            },
+        ]),
+    ].concat(htmlPlugins),
+};
 
-module.exports = function(env) {
-    if (env === 'production'){
-        return merge([
-            common,
-            extractCSS(),
-            uglifyJS()
-        ]);
-    }
-    if (env === 'development'){
-        return merge([
-            common,
-            devserver(),
-            sass(),
-            css()
-        ])
-    }
+module.exports = (env, options) => {
+    let production = options.mode === 'production';
+
+    conf.devtool = production
+                 ? 'source-map'
+                 : 'eval-sourcemap';
+
+    return conf;
 };
